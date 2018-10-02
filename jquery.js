865708344524f9b8ea8fbad237677ec069076356
tinablau <42932770+tinablau@users.noreply.github.com>
2018-10-02 11:10:04 +0200

    Tinas files Basic exercise
    we both tried to find separate solutions

---
+++ jquery.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+
+$('#bell').click(function(){ 
+    	$("#belld").text("The bell was clicked.");
+    	});
+$('#box').click(function(){ 
+    	$("#boxd").text("The box was clicked.");
+    	});
+$('#santa').click(function(){ 
+    	$("#santad").text("The santa was clicked.");
+    	});
+
+
+
+
+
+ })
+
